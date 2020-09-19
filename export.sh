54e7a9879284d4b2e5462cd160ba9e1c8c453e3f
psumanth <pernamitta.sumanth@harman.com>
2020-09-19 17:58:38 +0530

    This commit is shell script which exports the tool chain

---
+++ export.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH=~/work/drivers/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin:$PATH
+export TOOLCHAIN=~/work/drivers/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export ARCH=arm
+
